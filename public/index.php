00b578b918ce79d20b92fe447be66fcba9839cde
benke <benke@akon.cz>
2018-11-23 10:35:08 +0100

    docker done

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="DDD Course">
+    <meta name="author" content="Lukas Benke">
+    <title>DDD Course</title>
+</head>
+<body>
+<h1>DDD Course Workshop</h1>
+</body>
+</html>
